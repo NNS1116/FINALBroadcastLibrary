217614ca8c27ac1fbd1186038338b4aa8c3aa1ff
Nahlan <nahlan-nahla@outlook.com>
2024-10-10 00:16:00 -0400

    Add project files.

---
+++ FINALBroadcastLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BroadcastLibrary
+{
+    public class BroadcastMsg
+    {
+        public string BroadcastMessage()
+        {
+            return "This is a broadcast message!";
+        }
+    }
+}
